2958137540a521fe865ac08b89a1bd7729bb0440
Jm <jmdelasalas.dev@gmail.com>
2023-06-09 21:52:08 +0800

    Added font and update imports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import './globals/globals.css';
+import './globals/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
